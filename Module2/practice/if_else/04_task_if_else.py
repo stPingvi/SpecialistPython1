2aaa77eb11d7840b24d00a8fa55325854d64be67
stPingvi <96437341+stPingvi@users.noreply.github.com>
2021-12-21 12:42:44 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,12 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+number = int(input("Введите число ="))
+if number % 3 == 0 and number % 5 == 0:
+    print("Foobar")
+elif number % 3 == 0:
+    print("Foo")
+elif number % 5 == 0:
+    print("Bar")
+else:
+    print("Число не делится на 3 и 5")
